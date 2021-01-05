0d5815bc5bbee6349de773f36b6f2c7cb6c64099
tianchuang <1509760688@qq.com>
2021-01-05 12:38:03 +0800

    no message

--- app/Console/Commands/Events.php
+++ app/Console/Commands/Events.php
@@ -47,10 +47,10 @@ class Events extends Command
         }else{
             $min_block_timestamp=$NUpdatetime->updatetime+1;
         }
-        $dataTimestamp["updatetime"]=$min_block_timestamp+59;
+        $dataTimestamp2["updatetime"]=$min_block_timestamp+29;
         $dataTimestamp['datetime']=date("Y-m-d H:i:s",time());
         $allTrc20Transaction=array();
-        $Trc20TransactionUrl="https://api.trongrid.io/v1/contracts/".self::CONTRACT."/events?event_name=Transfer&min_block_timestamp=".$min_block_timestamp."000&max_block_timestamp=".$dataTimestamp["updatetime"]."000&limit=200";//正式服
+        $Trc20TransactionUrl="https://api.trongrid.io/v1/contracts/".self::CONTRACT."/events?event_name=Transfer&min_block_timestamp=".$min_block_timestamp."000&max_block_timestamp=".$dataTimestamp2["updatetime"]."000&limit=200";//正式服
         //$Trc20TransactionUrl="https://nile.trongrid.io/v1/contracts/".self::CONTRACT."/events?event_name=Transfer&min_block_timestamp=".$min_block_timestamp."000&max_block_timestamp=".$dataTimestamp["updatetime"]."000&limit=200";//nile测试服
         $Trc20Transaction=$this->GetTrc20Transaction($Trc20TransactionUrl);
         if(count($Trc20Transaction["data"])>0){
@@ -67,6 +67,26 @@ class Events extends Command
             }
         }
         DB::table('trc20_transactions')->insert($allTrc20Transaction);
+
+        $dataTimestamp["updatetime"]=$dataTimestamp2["updatetime"]+30;
+        $allTrc20Transaction2=array();
+        $Trc20TransactionUrl="https://api.trongrid.io/v1/contracts/".self::CONTRACT."/events?event_name=Transfer&min_block_timestamp=".$dataTimestamp2["updatetime"]."000&max_block_timestamp=".$dataTimestamp["updatetime"]."000&limit=200";//正式服
+        //$Trc20TransactionUrl="https://nile.trongrid.io/v1/contracts/".self::CONTRACT."/events?event_name=Transfer&min_block_timestamp=".$dataTimestamp2["updatetime"]."000&max_block_timestamp=".$dataTimestamp["updatetime"]."000&limit=200";//nile测试服
+        $Trc20Transaction=$this->GetTrc20Transaction($Trc20TransactionUrl);
+        if(count($Trc20Transaction["data"])>0){
+            foreach ($Trc20Transaction["data"] as $key => $value) {
+                $allTrc20Transaction2['block_number']=$value['block_number'];
+                $allTrc20Transaction2['block_timestamp']=$value['block_timestamp'];
+                $allTrc20Transaction2['contract_address']=$value['contract_address'];
+                $allTrc20Transaction2['from']='41'.substr($value['result']['from'],2);
+                $allTrc20Transaction2['to']='41'.substr($value['result']['to'],2);
+                $allTrc20Transaction2['value']=$value['result']['value'];
+                $allTrc20Transaction2['transaction_id']=$value['transaction_id'];
+                $allTrc20Transaction2['datetime']=date("Y-m-d H:i:s",time());
+                $allTrc20Transaction2[]=$allTrc20Transaction2;
+            }
+        }
+        DB::table('trc20_transactions')->insert($allTrc20Transaction2);
         DB::table('updatetime')->insert($dataTimestamp);
         //获取发推送
         $results = DB::select("SELECT
