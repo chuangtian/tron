491b8420c0503c362f62dcc9b63a66cf063b09ae
田闯 <1509760688@qq.com>
2021-03-04 03:26:16 +0800

    no message

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -22,8 +22,8 @@ class IndexController extends Controller
     public function index()
     {
 
-        $send=$this->send(config('app.activationAddress'),"41262f9bc8a1c04d2425e5a2fa02700c75e6a90575",4,config('app.activationAddressPrivateKey'));
-        dd($send);
+        $getNewblockUrl=self::FULL_NODE_API."/wallet/getnowblock";
+        dd($getNewblockUrl);
         
        
         $send=$this->send(config('app.activationAddress'),"41262f9bc8a1c04d2425e5a2fa02700c75e6a90575",1.1204,config('app.activationAddressPrivateKey'));
