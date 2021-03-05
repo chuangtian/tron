081fbcceae5894ca20949b735bcfb939412e5e2d
田闯 <1509760688@qq.com>
2021-03-05 22:20:47 +0800

    no message

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -166,7 +166,7 @@ class IndexController extends Controller
             }
             $info->value=bcdiv($info->value,$c,$tokeninfo->decimals);
             $key=md5($info->to.$info->contract_address.$info->transaction_id.$info->block_confirmations.$info->block_timestamp.$info->value.'Ual@wvsHsXFDQ8Vu'.'NcO%FJJf%8iALbof');
-            $url2 = 'https://client.rcmfx.com/erc_api?hash='.$info->transaction_id.'&to='.$info->to.'&api_key='.$key.'&time_stamp='.$info->block_timestamp.'&block_confirmations='.$info->block_confirmations.'&token='.$info->contract_address.'&value='.$info->value;
+            $url2 = 'https://sanduser.via-int.io/trc_api?hash='.$info->transaction_id.'&to='.$info->to.'&api_key='.$key.'&time_stamp='.$info->block_timestamp.'&block_confirmations='.$info->block_confirmations.'&token='.$info->contract_address.'&value='.$info->value;
             //$url2 = 'https://testclient.rcmfx.com/erc_api?hash='.$info->erc20_tx_hash.'&to='.$info->erc20_to.'&api_key='.$key.'&time_stamp='.$info->time_stamp.'&block_confirmations='.$info->block_confirmations.'&token='.$info->erc20_token.'&value='.$info->erc20_value;
             //dd($url2);
             $task_message2 = file_get_contents($url2);
