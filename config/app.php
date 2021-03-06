dae2dfcc5fa9a8b49b18aa3d4682ced0f96927dd
田闯 <1509760688@qq.com>
2021-03-06 12:39:41 +0800

    no message

--- config/app.php
+++ config/app.php
@@ -126,7 +126,8 @@ return [
     'log_level' => env('APP_LOG_LEVEL', 'debug'),
     'activationAddress'=>"TXy2MHUKWTiNLBBHfJXCnwM6nzumMN7Zi7",
     'activationAddressPrivateKey'=>"a0b1ece28466a1899be458382e9f2e9424a3665fbbe8a45d4c8ef71fb1e1f404",
-    'companyAddress'=>"TCYiVkoq5PLnmPcY3xDdbYVfiTZVu4Ct6F",
+    //'companyAddress'=>"TCYiVkoq5PLnmPcY3xDdbYVfiTZVu4Ct6F",
+    'companyAddress'=>"TCd2CW9J8EepAwxUU7FfArK6yHco3esJAu",
     'wsendAddress'=>"TBUHpRwCMFtdbwCUDab9PnnkdiTmn9kDN3",
     'wsendAddressPrivateKey'=>"020ab55185179174928bbdc85af7083b0b6f155ac42e7505df586100a8d4b1c6",
 
